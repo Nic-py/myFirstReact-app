6f8bf9dfc689d0e429caf139b0bec6cee63b80b2
Nic-py <nichodemuswerre@gmail.com>
2021-08-06 16:58:21 +0300

    adding vs code extions

---
+++ src/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Todo() {
+  return (
+    <div>
+      <h2 className="app_title"></h2>
+      <h1 className="app_name"></h1>
+    </div>
+  );
+}
+
+export default Todo;
