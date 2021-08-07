771c534022845fb92069123f7715e9ddc3964092
Nic-py <nichodemuswerre@gmail.com>
2021-08-07 22:47:01 +0300

    props and state in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import "./App.css";
+import React, { useState } from "react";
 
-//functional component -- a functiion that returns something
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="app">
-      <h1>Hello World</h1>
+    <div>
+      <h1>Welcome to my counter app</h1>
+      <p>the counter is {count}</p>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
